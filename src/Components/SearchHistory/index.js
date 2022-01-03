2707eaf840afb4b9a2a0498826388344d839f3d0
uttejgunda <thisisuttej@gmail.com>
2022-01-03 08:31:41 +0000

    2 test cases left

--- src/Components/SearchHistory/index.js
+++ src/Components/SearchHistory/index.js
@@ -1,59 +1,141 @@
 import {Component} from 'react'
 import './index.css'
-import "SearchHistoryItems" from '../SearchHistoryItems'
+import SearchHistoryItems from '../SearchHistoryItems'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
 
 class SearchHistory extends Component {
   state = {searchHistoryList: initialHistoryList, searchInput: ''}
 
   deleteItem = id => {
-      const{searchHistoryList} = this.state
-      const filteredSearchData = searchHistoryList.filter((eachItem) => (
-          eachItem.id !== id
-      ))
+    const {searchHistoryList} = this.state
+    const filteredList = searchHistoryList.filter(
+      eachItem => eachItem.id !== id,
+    )
+    this.setState({searchHistoryList: filteredList})
   }
 
   onChangeSearchInput = event => {
-      const {searchInput} =  this.state
-      const value = event.target.value
-      this.setState({searchInput: value})
+    this.setState({searchInput: event.target.value})
   }
 
   render() {
     const {searchHistoryList, searchInput} = this.state
-    const initialHistoryList = this.props
-    const updatedSearchHistory = searchHistoryList.filter(eachItem => 
-      eachItem.title.toLowerCase().includes(searchInput.toLowerCase())
+    const updatedSearchHistory = searchHistoryList.filter(eachItem =>
+      eachItem.title.toLowerCase().includes(searchInput.toLowerCase()),
     )
 
     return (
       <div className="bg-container">
         <nav className="nav-bar">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png "
-            alt="app logo"
-          />
+          <div className="nav-logo-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png "
+              alt="app logo"
+              className="logo"
+            />
+          </div>
           <div className="search-icon-container">
             <img
               src="https://assets.ccbp.in/frontend/react-js/search-img.png"
               alt="search"
+              className="search-icon"
             />
           </div>
           <input
-            type="text"
+            type="search"
             placeholder="Search history"
             className="search-bar"
-            onChange = {this.onChangeSearchInput}
+            onChange={this.onChangeSearchInput}
           />
         </nav>
-        <ul className="search-items-wrapper">
-          {updatedSearchHistory.map(eachItem => (
-            <SearchHistoryItems
-              key={eachItem.id}
-              details={eachItem}
-              deleteItem={this.deleteItem}
-            />
-          ))}
-        </ul>
+        {updatedSearchHistory === '' && (
+          <p className="no-results-text">There is no history to show</p>
+        )}
+        {updatedSearchHistory !== '' && (
+          <ul className="search-items-wrapper">
+            {updatedSearchHistory.map(eachItem => (
+              <SearchHistoryItems
+                key={eachItem.id}
+                details={eachItem}
+                deleteItem={this.deleteItem}
+              />
+            ))}
+          </ul>
+        )}
       </div>
     )
   }
--- src/Components/SearchHistoryItems/index.js
+++ src/Components/SearchHistoryItems/index.js
@@ -10,11 +10,18 @@ const SearchHistoryItems = props => {
 
   return (
     <li className="list-item">
-      <p className="time">{timeAccessed}</p>
-      <img src={logoUrl} className="logo" />
-      <p className="title">{title}</p>
-      <p className="domain">{domainUrl}</p>
-      <button className="delete-btn" onClick={onDelete}>
+      <div className="list-details">
+        <p className="time">{timeAccessed}</p>
+        <img src={logoUrl} className="logo" alt="domain logo" />
+        <p className="title">{title}</p>
+        <p className="domain">{domainUrl}</p>
+      </div>
+      <button
+        className="delete-btn"
+        testid="delete"
+        type="button"
+        onClick={onDelete}
+      >
         <img
           src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
           alt="delete"
